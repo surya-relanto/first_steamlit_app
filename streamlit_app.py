9a4f6f978022f3adc544d92e7ce4feadd7c4c859
surya-relanto <133542617+surya-relanto@users.noreply.github.com>
2023-11-12 00:30:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,17 +21,17 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.text("Fruityvice fruit advice")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')    
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.text("please select a fruit to get infromation")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-streamlit.header('Fruityvice Fruit Advice')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-#streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
@@ -46,15 +46,4 @@ add_fruit = streamlit.text_input('What fruit would you like information about?',
 streamlit.write('Thanks for adding ', add_fruit)
 my_cur.execute("insert into fruit_load_list values('from streamlist')")
 
-streamlit.text("Fruityvice fruit advice")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-  if not fruit_choice:
-    streamlit.text("please select a fruit to get infromation")
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
 
-except URLError as e:
-  streamlit.error()
